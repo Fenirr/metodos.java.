6ba33109a4a82e0ef7629dd5d71f08d437228c99
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 11:36:40 -0600

    con retorno sin parametros
    ejemplos de métodos en java

---
+++ ejemplo3.5.java
@@ -0,0 +1,18 @@
+package metodo3;
+
+public class ejemplo5 {
+public static void main(String args[]){	
+int resultado;
+resultado= multiplicacion();
+System.out.println("la multiplicacion es:"+resultado);
+}
+
+public static int multiplicacion(){
+int t=9, i=4;
+				
+int multiplicar=t*i;
+return multiplicar;
+
+	}
+	}
+
